c06d33ae463645cd0289b5cab08455efc302ad7f
wangcheng <feiyan35488@gmail.com>
2017-08-03 19:23:14 +0800

    添加lession14

---
+++ src/main/java/net/helao/leetcode/lession14/Solution.java
@@ -0,0 +1,45 @@
+package net.helao.leetcode.lession14;
+
+/**
+ * @author wangcheng<wangcheng@mucang.cn>
+ * @date 2017/8/3.
+ */
+public class Solution {
+
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        if (strs.length == 1) {
+            return strs[0];
+        }
+        String prefix = strs[0];
+        String s;
+        for (int i = 1; i < strs.length; i++) {
+            s = strs[i];
+            prefix = findCommonPrefix(prefix, s);
+            if (prefix.equals("")) {
+                return "";
+            }
+        }
+        return prefix;
+    }
+
+    public String findCommonPrefix(String a, String b) {
+        if (a.equals("") || b.equals("")) {
+            return "";
+        }
+        int len = Math.min(a.length(), b.length());
+        for (int i = 0; i < len; i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                return a.substring(0, i);
+            }
+        }
+        return len == a.length() ? a : b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().longestCommonPrefix(new String[]{"a", "bsd", "as"}));
+
+    }
+}
